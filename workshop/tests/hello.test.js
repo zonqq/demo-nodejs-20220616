9ccc1344d941969997304ac59901ef383d690069
up1 <somkiat.p@gmail.com>
2022-06-16 14:54:42 +0700

    Working with supertest

--- workshop/tests/hello.test.js
+++ workshop/tests/hello.test.js
@@ -1,3 +1,9 @@
-test('Hello first case', () => {
-  expect('pui').toBe('pui');
+const request = require("supertest");
+const app = require("../server");
+
+test('Hello World API', async () => {
+  const response = await request(app)
+      .get('/api')
+    expect(response.status).toEqual(200);
+    expect(response.text).toEqual('Welcome to TDD World');
 });
