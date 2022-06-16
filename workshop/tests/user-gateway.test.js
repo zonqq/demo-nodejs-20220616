159f914969d799ab35bcfd3f0b5da3568c9a900e
up1 <somkiat.p@gmail.com>
2022-06-16 16:13:52 +0700

    Working with nock

--- workshop/tests/user-gateway.test.js
+++ workshop/tests/user-gateway.test.js
@@ -1,7 +1,12 @@
+const nock = require('nock')
 const getAllUser = require("../gateways/user");
 
 test('Hello World API', async () => {
+  nock('https://jsonplaceholder.typicode.com')
+  .get('/users')
+  .reply(200, [{},{},{}]);
+
   const response = await getAllUser();
   expect(response.code).toEqual(200);
-  expect(response.data.length).toEqual(10);
+  expect(response.data.length).toEqual(3);
 });
